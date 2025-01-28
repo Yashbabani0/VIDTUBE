e97c1627f0a2eab1c72b338825d5a192cf3bb003
Yashbabani0 <yashbabani09@gmail.com>
2025-01-29 00:27:21 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,5 @@
 import express from "express";
-import path from "path";
-
-// Get the current directory name using import.meta.url
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const app = express();
-
-// Serve static files (like images, CSS, JavaScript) from the "public" folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// Serve the HTML file at the root URL
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { app } from "./app.js";
+import path from "path";
 
 const PORT = 7000;
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html")); // Change 'index.html' to match the actual path
 });
